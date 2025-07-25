91076057305d083c36cd4c299c0775cb81317372
rodolfofernandes95 <rodolfo.fernandes@epamig.br>
2025-07-25 10:59:27 -0300

    foto

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     serverComponentsExternalPackages: ['mysql2']
-  }
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'epamigsistema.com',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
